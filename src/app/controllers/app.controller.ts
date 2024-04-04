90df11d9472f9478b4963f984d51a9b9c4f05a50
BlueMadness27 <ashley654v@gmail.com>
2024-04-04 14:39:39 -0500

    adding and changing files

--- src/app/controllers/app.controller.ts
+++ src/app/controllers/app.controller.ts
@@ -1,11 +1,12 @@
 import { Request, Response, Router } from "express";
 import { pino } from 'pino';
+import { UserService } from "../services/user.service";
 
 export class AppController {
   public router: Router = Router();
   private log: pino.Logger = pino();
 
-  constructor() {
+  constructor(private userService: UserService) {
     this.initializeRouter();
   }
 
@@ -24,6 +25,27 @@ export class AppController {
       req.session.destroy(() => {
         res.redirect("/");
       });
+    });
+
+    this.router.get("/signup", function (req, res) {
+      res.render("signup");
+    });
+
+    this.router.post("/signup", async (req: any, res) => {
+      const user = await this.userService.createUser(req.body.username, req.body.email, req.body.password);
+      req.session.user = user;
+      res.redirect("/");
+    });
+
+    // Handle login form submissions
+    this.router.post("/Login", async (req: any, res) => {
+      const user = await this.userService.authenticateUser(req.body.username, req.body.password);
+      if (user) {
+        req.session.user = user;
+        res.redirect("/");
+      } else {
+        res.status(401).send("Invalid username or password");
+      }
     });
 
     //PROTECT THE HOMEPAGE
